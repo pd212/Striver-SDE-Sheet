7e852fdaf50ddec0a86cc0fb443ac96afb160fc3
MyProjects <deshpandepranav47@gmail.com>
2023-01-06 15:16:25 +0530

    first

---
+++ 2Sum.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+class PairSum{
+    public int[] twoSum(int[] arr, int target) {
+        int n = arr.length;
+        int ans[]=new int[2];
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(hm.containsKey(target - arr[i])){
+                ans[0]=hm.get(target -arr[i]);
+                ans[1]= i;
+                return ans;
+            }
+            else{
+                hm.put(arr[i], i);
+            }
+        }
+        return ans;
+    }
+}
