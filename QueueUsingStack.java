98eb3d81b15aaeb012be20ef3fc28992d1195b41
MyProjects <deshpandepranav47@gmail.com>
2023-01-20 20:30:29 +0530

    https://www.codingninjas.com/codestudio/problems/queue-using-stack_799482?topList=striver-sde-sheet-problems&leftPanelTab=0

---
+++ QueueUsingStack.java
@@ -0,0 +1,54 @@
+import java.lang.*;
+import java.util.*;
+public class Queue {
+    // Define the data members(if any) here.
+    Stack<Integer>s1;
+    Stack<Integer>s2;
+    Queue() {
+        s1=new Stack<>();
+        s2=new Stack<>();
+        // Initialize your data structure here.
+    }
+
+    void enQueue(int val) {
+        // Implement the enqueue() function.
+        s1.push(val);
+    }
+
+    int deQueue() {
+        // Implement the dequeue() function.
+        if(s1.isEmpty() && s2.isEmpty()){
+            return -1;
+        }
+        else if(!s2.empty()){
+            return s2.pop();
+        }
+        else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return s2.pop();
+        }
+    }
+
+    int peek() {
+        // Implement the peek() function here.
+         if(s1.isEmpty() && s2.isEmpty()){
+            return -1;
+         }
+        else if(!s2.empty()){
+            return s2.peek();
+        }
+        else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return s2.peek();
+        }
+    }
+
+    boolean isEmpty() {
+        // Implement the isEmpty() function here.
+        return (s1.isEmpty() && s2.isEmpty());
+    }
+}
