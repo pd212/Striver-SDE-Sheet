8601b2bebf44dd97db062c831c38175fb5831770
MyProjects <deshpandepranav47@gmail.com>
2023-01-21 21:26:15 +0530

    https://www.codingninjas.com/codestudio/problems/stack-implementation-using-array_3210209?topList=striver-sde-sheet-problems&leftPanelTab=0

--- 2Sum.java
+++ 2Sum.java
@@ -5,10 +5,9 @@ import java.util.Stack;
 
 class PairSum{
     public int[] twoSum(int[] arr, int target) {
-        int n = arr.length;
         int ans[]=new int[2];
         HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<arr.length;i++){
             if(hm.containsKey(target - arr[i])){
                 ans[0]=hm.get(target -arr[i]);
                 ans[1]= i;
