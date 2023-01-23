82ea0712e4a6f1fb28081f0f6aac3c8562e8ee84
MyProjects <deshpandepranav47@gmail.com>
2023-01-23 18:08:58 +0530

    https://www.codingninjas.com/codestudio/problems/sort-a-stack_985275?topList=striver-sde-sheet-problems&leftPanelTab=0

---
+++ SortAStack.java
@@ -0,0 +1,26 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+
+	public static void sorted(Stack<Integer>st,int x){
+		if(st.empty() || x > st.peek()){
+			st.push(x);
+			return;
+		}
+		int top = st.pop();
+		sorted(st,x);
+		st.push(top);
+	}
+	public static void sortStack(Stack<Integer> stack) {
+		// Write your code here.
+		if(!stack.empty()){
+			int n = stack.pop();
+
+			sortStack(stack);
+
+			sorted(stack,n);
+		}
+	}
+
+}
